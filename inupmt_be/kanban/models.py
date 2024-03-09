6ced15d6128c377a2a21c629480b24d6bedefe1d
Burak Özcan <burakozcan156@hotmail.com>
2024-03-09 13:53:32 +0300

    assignees verisi veritabanına requests'den çekilip veriliyor

--- inupmt_be/kanban/models.py
+++ inupmt_be/kanban/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from enum import IntEnum
 
-class Priority(IntEnum):
-    LOW = 1
-    MEDIUM = 2
-    HIGH = 3
+class Priority(models.IntegerChoices):
+    LOW = 1, 'Düşük'
+    MEDIUM = 2, 'Orta'
+    HIGH = 3, 'Yüksek'
 
 class Task(models.Model):
     reporter = models.ForeignKey(User, related_name='reported_tasks', on_delete=models.CASCADE)
-    assignee = models.ForeignKey(User, related_name='assigned_tasks', on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(User, related_name='assigned_tasks')
     labels = models.CharField(max_length=100)
     due_date = models.DateField()
     create_time = models.DateTimeField(auto_now_add=True)
--- inupmt_be/kanban/serializers.py
+++ inupmt_be/kanban/serializers.py
@@ -5,3 +5,9 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+        
+    def create(self, validated_data):
+        assignees_data = validated_data.pop('assignees')
+        instance = Task.objects.create(**validated_data)
+        instance.assignees.set(assignees_data)
+        return instance
